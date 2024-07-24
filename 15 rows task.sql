e0b1825b03f474ab6c80fdea70726ae430150b61
akansha2104 <akanshagaikwad2020@gmail.com>
2024-07-25 00:02:38 +0530

    there  is one simple task done of inserting 15 row manually and another task is by creating subject table with different fields related to  subjects manually and with the help of copy from csv header file

---
+++ 15 rows task.sql
@@ -0,0 +1,45 @@
+create table student(
+	id int primary key,
+	name varchar,
+	age int,
+	college_id int
+)
+
+select * from student
+
+
+
+insert into student values (1,'name1', 26,1)
+
+select * from student
+
+insert into student values (2,'name2', 34,2)
+
+select * from student
+
+insert into student values (3,'name3',36,1) , (4,'name4', 41,4)
+
+select * from student
+
+
+select name from student
+
+select name, age from student
+
+insert into student values (5,'name4',27,5)
+
+insert into student values (6,'name6',25,6)
+
+insert into student values (7,'name7',18,7)
+
+select * from student
+
+insert into student values (8,'name8',22,8)
+
+insert into student values (9,'name9',21,9)
+
+insert into student values (10,'name10',23,10)
+
+insert into student values (11,'name11',24,11) , (12,'name12', 18,11) , (13,'name13', 18,14)
+
+insert into student values (14,'name14',25,9) , (15,'name15',26,16)
---
+++ task1_query.sql
@@ -0,0 +1,43 @@
+select * from subjects
+
+insert into subjects values (9,'physics',5,4,2,'medium')
+
+insert into subjects values (11,'biology',1,7,8,'easy'), (12,'environmental studies',1,2,8,'medium') , (13,'history',2,10,1,'hard')
+	
+insert into subjects values (14,'sociology',2,3,4,'easy'), (15,'literature',4,2,5,'medium') , (16,'DBMS',2,10,1,'hard')
+
+select * from subjects
+
+insert into subjects values (14,'sociology',2,3,4,'easy'), (15,'literature',4,2,5,'medium') , (16,'DBMS',2,10,1,'hard')
+
+insert into subjects values (17,'geogarphy',2,3,4,'easy'), (18,'economics',1,3,5,'medium') , (19,'algebra',5,10,1,'hard')
+
+insert into subjects values (20,'latin',1,3,8,'hard'), (21,'computer network',6,3,5,'medium') , (22,'geometry',5,10,1,'hard')
+
+(23,'trignometry',5,2,8,'hard'), (24,'data science',6,1,8,'easy') , (26,'anatomy',1,1,1,'hard')
+
+insert into subjects values (31,'robotics',1,3,8,'hard'), (32,'computer science',6,3,5,'medium') , (33,'machine learning',5,10,1,'hard')
+
+insert into subjects values (34,'biotechnology',6,1,3,'easy'), (35,'cell biology',6,1,8,'easy') , (36,'cybersecurity',5,4,1,'hard')
+
+insert into subjects values (37,'marketing',4,1,2,'medium'), (38,'finance',6,7,4,'easy') , (39,'accounting',2,6,1,'hard')
+
+insert into subjects values (40,'nutrition',2,5,8,'easy'), (41,'medicine',6,3,5,'easy') , (42,'pharmacy',1,15,4,'medium') ,
+(43,'law',5,2,8,'hard'), (44,'criminology',4,2,8,'medium') , (45,'social work',2,1,3,'easy') , (46,'logic',2,3,8,'hard'), (47,'calculas',4,6,5,'medium') , (48,'statitics',4,5,11,'easy') ,
+(50,'management',4,2,8,'hard'), (51,'physiotherapy',7,5,8,'easy') , (52,'graphic design',1,2,2,'easy') , (53,'nuclear study',2,7,12,'easy'), (54,'computer architecture',5,3,5,'easy') , (55,'physical education',5,9,1,'hard'),
+(56,'health education',4,2,8,'easy'), (57,' rocket science',4,2,1,'easy') , (58,'urban planning',5,7,1,'hard')
+
+insert into subjects values (59,'art history',1,6,8,'medium'), (60,'music theory',4,5,6,'hard') , 
+(61,'dance studies',5,7,4,'medium') ,(62,'journalism',2,3,4,'easy'), (63,'creative writing',3,2,8,'medium') ,
+(64,'SEPM',4,1,3,'easy') , (65,'computational theory',7,3,8,'easy'), (66,'immunology',7,6,1,'hard') , (67,'neuroscience',7,5,1,'medium') ,
+(68,'management',4,2,8,'hard'), (69,'human resources',2,3,4,'medium') , (70,'ecology',4,8,2,'hard') , 
+(71,'biochemistry',4,7,6,'easy'), (72,'civil engg',2,3,5,'medium') , (73,'physiology',5,9,1,'easy')
+
+select * from subjects
+
+insert into subjects values (74,'differential equation',1,6,7,'hard'), (75,'chemical engg',4,5,6,'easy') , (76,'computer technology',2,7,1,'easy')
+
+copy subjects from 'D:\data analytics\sql\DAY3\subject_task1_output.csv' delimiter ',' csv header
+
+select * from subjects
+
